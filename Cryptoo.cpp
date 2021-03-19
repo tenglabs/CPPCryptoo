9af0184dcc4dbe08637577dde30504e2d846aa19
tenglabs <tengoneotechnologies@gmail.com>
2021-03-19 22:24:23 +0200

    I hate neighbours

--- Cryptoo.cpp
+++ Cryptoo.cpp
@@ -73,7 +73,7 @@ size_t Block::getPreviousHash()
 
 bool Block::isHashVallid()
 {
-	return generateHash;
+	return generateHash() == blockHash ;
 
 }
 
@@ -90,9 +90,86 @@ public:
 	vector<Block> chain;
 	Blockchain();
 	void addBlock(TransactionData data);
-	void isChainValid();
+	bool isChainValid();
 
 	Block* getLatestBlock();
 
 
 };
+
+
+Blockchain::Blockchain()
+{
+	Block genesis = createGenesisBlock();
+	chain.push_back(genesis);
+
+}
+
+
+Block Blockchain::createGenesisBlock()
+{
+	time_t current;
+	TransactionData d;
+	d.amount = 0;
+	d.receiverKey = "None";
+	d.senderKey = "None";
+	d.timestamp = time(&current);
+
+	hash<int> hash1;
+	Block genesis(0, d, hash1(0));
+	return genesis;
+
+}
+
+Block *Blockchain::getLatestBlock()
+{
+	return &chain.back();
+}
+
+void Blockchain::addBlock(TransactionData d)
+{
+
+	int index = (int)chain.size() - 1;
+	Block newBlock(index, d, getLatestBlock()->getHash());
+
+}
+
+bool Blockchain::isChainValid()
+{
+
+	vector <Block>::iterator it;
+	int chainLen = (int)chain.size();
+
+	for (it = chain.begin(); it != chain.end(); ++it)
+	{
+		Block currentBlock = *it;
+		if (!currentBlock.isHashVallid())
+		{
+			return false;
+
+		}
+		if (chainLen > 1)
+		{
+			Block previousBlock = *(it - 1);
+			if (currentBlock.getPreviousHash() != previousBlock.getHash())
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	Blockchain PotatoCoin;
+
+
+	TransactionData data1;
+	time_t data1Time;
+	data1.amount = 1.5;
+	data1.receiverKey = "JohnDoe";
+	data1.senderKey = "Potato";
+	data1.timestamp = time(&data1Time);
+}
+
